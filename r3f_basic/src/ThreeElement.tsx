cadb90fc2d25290f52b4d13d2c83c0e7e5674035
Dova <heetask@gmail.com>
2026-01-29 08:38:39 +0900

    R3F 설명 - Renderer ( useFrame )

--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,10 +1,26 @@
 import * as THREE from 'three';
+import { useThree, useFrame } from '@react-three/fiber'
+import { useRef } from 'react';
 
 export default function ThreeElement() {
+  const {size, gl, scene, camera} = useThree();
+  const boxRef = useRef<THREE.Mesh>(null);
+
+  // scene에 대한 변화가 일어난 것들은 모두 useFrame 안에 넣어 다시 렌더링을 해줘야 제대로 동작을 한다.
+  useFrame((state, delta) => {
+    // console.log('state: ', state);
+    // console.log('delta: ', delta); // 렌더링 프레임 간의 시간 간격
+    // console.log('boxRef: ', boxRef);
+    
+    boxRef.current.rotation.x += delta; // 지나가는 시간만큼 rotation 각도가 추가 => 상자가 일정한 속도로 x축으로 회전
+    // boxRef.current.position.y -= 0.01; // 지나가는 시간만큼 position y 값이 추가 => 상자가 일정한 속도로 0.01씩 아래로 이동 
+    // boxRef.current.scale.z += 0.01; // z축 스케일이 점점 커짐
+  })
+  
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh rotation={[THREE.MathUtils.degToRad(45),THREE.MathUtils.degToRad(45),0]}>
+      <mesh ref={boxRef} rotation={[THREE.MathUtils.degToRad(45),THREE.MathUtils.degToRad(45),0]}>
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
