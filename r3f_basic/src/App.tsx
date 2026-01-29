4ed8b2d22befdfa2a7dbe64fdec2ffa5a45642bc
Darock <heetask@gmail.com>
2026-01-29 13:53:59 +0900

    R3F 설명 - Controls, Helper ( axesHelper, gridHelper )

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
 import ThreeElement from './ThreeElement'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
@@ -9,10 +10,20 @@ function App() {
         camera={{
           fov: 75,
           near: 1,
-          far: 20,
+          far: 100,
           position: [3,3,0]
         }}
       >
+        {/* Math.PI = 180 */}
+        <OrbitControls
+          // minAzimuthAngle={-Math.PI / 4}
+          // maxAzimuthAngle={Math.PI / 4}
+          // minPolarAngle={Math.PI / 6}
+          // maxPolarAngle={Math.PI - Math.PI / 6}
+        />
+        {/* args에 들어가는 기본 단위: m */}
+        <axesHelper args={[6]} />
+        <gridHelper args={[10, 10]} />
         <ThreeElement />
       </Canvas>
     </>
