d2ab2f8b37b97565e5097e40ffef3f09de2b3e22
Darock <heetask@gmail.com>
2026-01-30 11:18:58 +0900

    Object 3D - transformation 위치, 크기, 회전

--- r3f_basic/src/App.tsx
+++ r3f_basic/src/App.tsx
@@ -2,17 +2,8 @@ import './App.css'
 import { Canvas } from '@react-three/fiber'
 import ThreeElement from './ThreeElement'
 import { OrbitControls } from '@react-three/drei'
-import { useControls } from 'leva'
 
 function App() {
-  const color = useControls({
-    value: 'white',
-  })
-
-  const grid = useControls({
-    segmant: {value: 10, min: 2, max: 100, step: 1}
-  })
-
   return (
     <>
       <Canvas
@@ -20,10 +11,10 @@ function App() {
           fov: 75,
           near: 1,
           far: 100,
-          position: [3,3,0]
+          position: [5,5,5]
         }}
       >
-        <color attach="background" args={[color.value]} />
+        <color attach="background" args={['white']} />
         {/* Math.PI = 180 */}
         <OrbitControls
           // minAzimuthAngle={-Math.PI / 4}
@@ -33,7 +24,8 @@ function App() {
         />
         {/* args에 들어가는 기본 단위: m */}
         <axesHelper args={[6]} />
-        <gridHelper args={[10, grid.segmant]} />
+        {/* args={[그리드의 크기, 그리드를 나누는 개수]} */}
+        <gridHelper args={[10, 10]} />
         <ThreeElement />
       </Canvas>
     </>
--- r3f_basic/src/ThreeElement.tsx
+++ r3f_basic/src/ThreeElement.tsx
@@ -1,14 +1,11 @@
 import * as THREE from 'three';
 import { useThree, useFrame } from '@react-three/fiber'
 import { useRef } from 'react';
-import { useControls } from 'leva'
 
 export default function ThreeElement() {
   const {size, gl, scene, camera} = useThree();
   const boxRef = useRef<THREE.Mesh>(null);
-  const box = useControls({
-    rotation: {value: 0, min: -360, max: 360, step: 1}
-  })
+
 
   // scene에 대한 변화가 일어난 것들은 모두 useFrame 안에 넣어 다시 렌더링을 해줘야 제대로 동작을 한다.
   useFrame((state, delta) => {
@@ -16,7 +13,7 @@ export default function ThreeElement() {
     // console.log('delta: ', delta); // 렌더링 프레임 간의 시간 간격
     // console.log('boxRef: ', boxRef);
     
-    // boxRef.current.rotation.x += delta; // 지나가는 시간만큼 rotation 각도가 추가 => 상자가 일정한 속도로 x축으로 회전
+    boxRef.current.rotation.x += delta; // 지나가는 시간만큼 rotation 각도가 추가 => 상자가 일정한 속도로 x축으로 회전
     // boxRef.current.position.y -= 0.01; // 지나가는 시간만큼 position y 값이 추가 => 상자가 일정한 속도로 0.01씩 아래로 이동 
     // boxRef.current.scale.z += 0.01; // z축 스케일이 점점 커짐
   })
@@ -24,7 +21,16 @@ export default function ThreeElement() {
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh ref={boxRef} rotation={[THREE.MathUtils.degToRad(45),THREE.MathUtils.degToRad(box.rotation),0]}>
+      <mesh 
+        ref={boxRef} 
+        position={[0,0,0]} 
+        scale={[1,1,1]}
+        rotation={[
+          THREE.MathUtils.degToRad(0),
+          THREE.MathUtils.degToRad(0),
+          THREE.MathUtils.degToRad(45),
+        ]}
+      >
         {/* geometry: 모양
         material: 색상 */}
         <boxGeometry />
